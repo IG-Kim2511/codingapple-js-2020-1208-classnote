7ede32c1d842debf3b958f17b3da54054d1d5d67
iikim <iikim2511@gmail.com>
2021-01-15 13:36:22 -0500

    c19

--- script.js
+++ script.js
@@ -953,7 +953,7 @@ function 글자세기(글){
   글자세기('aaccbbb');
 
 
-// 🦄🦄c19 Reference data type: array, object (Primitive data type : string, number)
+// 🦄🦄c19 Reference data type - array, object (Primitive data type - string, number) 
 console.log('🦄🦄🦄🦄c19')
 
 /* 2)
